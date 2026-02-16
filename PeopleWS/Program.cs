d0e8a7f40e0d1ffe9a7dc7953fd718aaba15bde2
Stefan Lebelt <stefan.lebelt@exedio.com>
2026-02-16 12:56:27 +0100

    added required endpoints (Task: WS: Setup project)

--- PeopleWS/Program.cs
+++ PeopleWS/Program.cs
@@ -1,5 +1,29 @@
+
+using Microsoft.AspNetCore.Http.HttpResults;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+app.MapGet("/person/{id}", Results<Ok<Person>, NotFound> (int id) =>
+{
+	Person person = null;
+	//TODO implement search
+	return person is null ? TypedResults.NotFound() : TypedResults.Ok(person);
+});
+app.MapGet("/person/search{term}", Results<Ok<List<Person>>, NotFound> (string term) =>
+{
+	List<Person> Result = [];
+	//TODO implement search
+	return Result.Count == 0 ? TypedResults.NotFound() : TypedResults.Ok(Result);
+});
+app.MapPost("person/", (Person person) =>
+{
+	//TODO implement creation or update
+	return TypedResults.Created("/person/{id}, person");
+});
 
 app.Run();
+
+public record Phone(int CountryPrefix, int RegionPrefix, int Number, string phoneType);
+public record PostalAddress(string PostalCode, string Town, string Street, string StreetNumber);
+public record Person(int ID, string FirstName, string LastName, DateTime DataOfBirth, List<PostalAddress> PostalAddresses, List<Phone> PhoneNumbers);
