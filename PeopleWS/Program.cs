9f42b6919c4da6adf01a42a33752b022d4242c61
Stefan Lebelt <stefan.lebelt@exedio.com>
2026-02-17 10:32:49 +0100

    added console-logging-outputs to the ws (Task: GUI: call webservices)

--- PeopleWS/Program.cs
+++ PeopleWS/Program.cs
@@ -6,12 +6,14 @@ var app = builder.Build();
 
 app.MapGet("/person/{id}", Results<Ok<Person>, NotFound> (int id) =>
 {
+	Console.WriteLine("GET-call to /person/" + id.ToString());
 	Person person = new Person("1", "Stefan", "Lebelt", new DateOnly(year: 1981, month: 1, day: 19), [new PostalAddress("01728", "Bannewitz", "Südhang", "20")], []);
 	//TODO implement search
 	return person is null ? TypedResults.NotFound() : TypedResults.Ok(person);
 });
 app.MapGet("/search/{term}", Results<Ok<List<Person>>, NotFound> (string term) =>
 {
+	Console.WriteLine("GET-call to /search/" + term.ToString());
 	List<Person> Result = [];
 	Result.Add(new Person("1", "Stefanus", "Lebelt", new DateOnly(year: 1981, month: 1, day: 19), [new PostalAddress("01728", "Bannewitz", "Südhang", "20"), new PostalAddress("02708", "Großschweidnitz", "Fliederweg", "19")], [new Phone(49, 172, 6004535, "Mobile"), new Phone(49, 175, 6615393, "Mobile Work")]));
 	//TODO implement search
@@ -19,6 +21,7 @@ app.MapGet("/search/{term}", Results<Ok<List<Person>>, NotFound> (string term) =
 });
 app.MapGet("/all", Results<Ok<List<Person>>, NotFound> () =>
 {
+	Console.WriteLine("GET-call to /all");
 	List<Person> Result = [];
 	Result.Add(new Person("1", "Stefan", "Lebelt", new DateOnly(year: 1981, month: 1, day: 19), [new PostalAddress("01728", "Bannewitz", "Südhang", "20"), new PostalAddress("02708", "Großschweidnitz", "Fliederweg", "19")], [new Phone(49, 172, 6004535, "Mobile"), new Phone(49, 175, 6615393, "Mobile Work")]));
 	//TODO implement search
@@ -26,6 +29,7 @@ app.MapGet("/all", Results<Ok<List<Person>>, NotFound> () =>
 });
 app.MapPost("person/", (Person person) =>
 {
+	Console.WriteLine("POST-call to /person/");
 	//TODO implement creation or update
 	return TypedResults.Created("/person/{id}, person");
 });
