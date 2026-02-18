9153bf842402f2f4ceab28042f0482ba815ed8a9
Stefan Lebelt <stefan.lebelt@exedio.com>
2026-02-18 19:22:41 +0100

    auto-load related adresses and phones (Task: WS: Get/Search Data)

--- PeopleWS/Program.cs
+++ PeopleWS/Program.cs
@@ -4,16 +4,15 @@ using Microsoft.AspNetCore.Http.HttpResults;
 using PeopleWS.DB.Models;
 using PeopleWS.DB;
 using PeopleWS.WsModels;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/person/{id}", Results<Ok<PersonWS>, NotFound> (int id) =>
+app.MapGet("/person/{id}", async Task<Results<Ok<PersonWS>, NotFound>> (int id) =>
 {
 	Console.WriteLine("GET-call to /person/" + id.ToString());
-	//PersonWS personWS = new PersonWS("1", "Stefan", "Lebelt", new DateOnly(year: 1981, month: 1, day: 19), [new PostalAddressWS("01728", "Bannewitz", "Südhang", "20")], [new PhoneWS(49, 172, 6004535, "Mobile"), new PhoneWS(49, 175, 6615393, "Mobile Work")]);
-
-	Person person = new SchleupenHomeworkContext().People.Where(p => p.PersonId == id).FirstOrDefault();
+	Person? person = new SchleupenHomeworkContext().People.Include(person => person.Addresses).Include(person => person.Phones).Where(p => p.PersonId == id).FirstOrDefault();
 	if (person == null)
 	{
 		return TypedResults.NotFound();
@@ -27,7 +26,7 @@ app.MapGet("/search/{term}", Results<Ok<List<PersonWS>>, NotFound> (string term)
 {
 	Console.WriteLine("GET-call to /search/" + term.ToString());
 	List<PersonWS> Result = [];
-	foreach( Person person in new SchleupenHomeworkContext().People.Where(p => p.FirstName == term || p.LastName == term) )
+	foreach (Person person in new SchleupenHomeworkContext().People.Include(person => person.Addresses).Include(person => person.Phones).Where(p => p.FirstName == term || p.LastName == term))
 	{
 		Result.Add(PersonWS.FromDbModel(person));
 	}
@@ -37,7 +36,7 @@ app.MapGet("/all", Results<Ok<List<PersonWS>>, NotFound> () =>
 {
 	Console.WriteLine("GET-call to /all");
 	List<PersonWS> Result = [];
-	foreach( Person person in new SchleupenHomeworkContext().People )
+	foreach (Person person in new SchleupenHomeworkContext().People.Include(person => person.Addresses).Include(person => person.Phones))
 	{
 		Result.Add(PersonWS.FromDbModel(person));
 	}
